2f6cece74e1524ddad2a4b099638d258be1976d8
kkevinmartinezz <kevin425.m@hotmail.com>
2026-02-03 15:34:58 -0500

    Problem 6 - initial commit with tests in place.

---
+++ problems_1_100/1-10/6_zigzag_conversion.py
@@ -0,0 +1,12 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        pass
+
+def main():
+    solution = Solution()
+    print(solution.convert("PAYPALISHIRING", 3))
+    print(solution.convert("PAYPALISHIRING", 4))
+    print(solution.convert("PAYPALISHIRING", 1))
+
+if __name__ == '__main__':
+    main()
